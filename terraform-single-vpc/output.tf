ca6484fd76ed55903233ddcb0094aa89059b9124
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-07-03 13:40:51 +0200

    Update output.tf

--- terraform-single-vpc/output.tf
+++ terraform-single-vpc/output.tf
@@ -34,6 +34,14 @@ output "jumbox_public_ip" {
    value = "${aws_instance.jumpbox.public_ip}"
 }
 
+output "demo2_private_ip" {
+   value = "${aws_instance.jumpbox.public_ip}"
+}
+
+output "demo3_private_ip" {
+   value = "${aws_instance.jumpbox.public_ip}"
+}
+
 output "private_key" {
   value     = tls_private_key.key.private_key_pem
   sensitive = true
